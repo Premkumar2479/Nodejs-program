a9a08264d9033536d0a706618e8ea977acc9ee77
Premkumar2479 <pk2335301@gmail.com>
2025-09-26 14:19:40 +0530

    add text

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let n =5;
+// let n =5;
 
-for(let i=0; i<n; i++) {
-    console.log("Hello,",i);
-}
-console.log("Done");
+// for(let i=0; i<n; i++) {
+//     console.log("Hello,",i);
+// }
+// console.log("Done");
+
+
+console.log(process.argv);
